2cfa80a88b2c41b9b16602d906b6eea00f6634e0
Milena Rojas <47427764+milerojas22@users.noreply.github.com>
2019-04-25 09:59:44 -0500

    Add files via upload

---
+++ taller.py
@@ -0,0 +1,8 @@
+lista=["Uno","Dos","Tres","Cuatro","Cinco","Seis","Siete","Ocho"]
+dict2={"nombre":"Milena","apellido":"Rojas","telefono":"321","edad":"25"}
+
+dict1={"lista":lista,"String":"Sexo","Diccionario2":dict2}
+
+for x in dict1:
+	print (x," : ", dict1[x])
+
